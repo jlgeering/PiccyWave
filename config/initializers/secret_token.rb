bbfa3ad5d3375198bfd0dd4210475b883f96f518
Jean-Luc Geering <jlgeering.12@gmail.com>
2012-02-23 22:21:57 +0100

    spatz a fait un site web

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PiccyWave::Application.config.secret_token = '64cda35f0fc515a0087453df123f4c2ea0469b30564ba2924aa30c85c914989ce7e30744957158db13656ed1109e24854ba136222b893c2190720dc67c15fd15'
